e77f52cb2b41c23e2043d38009869b7f8135be3d
Drissdo185 <dothanhdat185@gmail.com>
2024-05-31 15:05:54 +0700

    hehe

--- README.md
+++ README.md
@@ -11,28 +11,27 @@ CREATE SCHEMA IF NOT EXISTS mlek2.bronze
 WITH (location = 's3://bronze-data/');
 
 -- Create the table within the schema
-CREATE TABLE IF NOT EXISTS mlek2.bronze.nyc_taxi (
-  congestion_surcharge DOUBLE,
-  dolocationid INT,
-  dropoff_datetime TIMESTAMP,
-  extra DOUBLE,
-  fare_amount DOUBLE,
-  improvement_surcharge DOUBLE,
-  mta_tax DOUBLE,
-  passenger_count INT,
-  payment_type INT,
-  pickup_datetime TIMESTAMP,
-  pulocationid INT,
-  ratecodeid DOUBLE,
-  store_and_fwd_flag VARCHAR(30),
-  tip_amount DOUBLE,
-  tolls_amount DOUBLE,
-  total_amount DOUBLE,
-  trip_distance DOUBLE,
-  vendorid INT
+CREATE TABLE IF NOT EXISTS mlek2.bronze.banking_data (
+  age INT,
+  job VARCHAR(30),
+  marital VARCHAR(10),
+  education VARCHAR(10),
+  default VARCHAR(3),
+  balance INT,
+  housing VARCHAR(3),
+  loan VARCHAR(3),
+  contact VARCHAR(10),
+  day INT,
+  month VARCHAR(3),
+  duration INT,
+  campaign INT,
+  pdays INT,
+  previous INT,
+  poutcome VARCHAR(10)
 ) WITH (
   external_location = 's3://bronze-data/raw_data',
   format = 'PARQUET'
 );
+
 
 ```
--- batch_processing/script/bronze_to_silver.py
+++ batch_processing/script/bronze_to_silver.py
@@ -1,18 +1,15 @@
 from minio import Minio
-from glob import glob
-import os
 from pyspark.sql import SparkSession
-
 from helpers import load_cfg 
 
 CFG_FILE = "config.yaml"
+
 def main():
     cfg = load_cfg(CFG_FILE)
     lakehouse_cfg = cfg["minio_lakehouse"]
-    raw_data_cfg = cfg["raw_data"]
+    datalake_cfg = cfg["minio_datalake"]
 
-    # Create a client with the MinIO server playground, its access key
-    # and secret key.
+    # Create a client with the MinIO server playground, its access key and secret key.
     client = Minio(
         endpoint=lakehouse_cfg["endpoint"],
         access_key=lakehouse_cfg["access_key"],
@@ -27,18 +24,24 @@ def main():
     else:
         print(f'Bucket {lakehouse_cfg["bucket_name"]} already exists, skip creating!')
 
-    ## Spark session
-    spark = (SparkSession.builder
-         .appName("prestudy")
-         .config("spark.hadoop.fs.s3a.access.key", lakehouse_cfg["access_key"])
-         .config("spark.hadoop.fs.s3a.secret.key", lakehouse_cfg["secret_key"])
-         .config("spark.hadoop.fs.s3a.path.style.access", "true")
-         .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-         .config("spark.hadoop.fs.s3a.endpoint", "http://localhost:9000")
-         .getOrCreate())
-    
+    # Spark session
+    spark = (SparkSession.builder.master("local")
+             .appName("Bronze to Silver")
+             .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.1,com.amazonaws:aws-java-sdk-bundle:1.11.375")
+             .config("spark.hadoop.fs.s3a.access.key", datalake_cfg["access_key"])
+             .config("spark.hadoop.fs.s3a.secret.key", datalake_cfg["secret_key"])
+             .config("spark.hadoop.fs.s3a.path.style.access", "true")
+             .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+             .config("spark.hadoop.fs.s3a.endpoint", "http://localhost:9000")
+             .getOrCreate())
+
     # Read data
-    df = spark.read.parquet(f"s3a://{lakehouse_cfg['bucket_name']}/{lakehouse_cfg['folder_name']}")
+    df = spark.read.parquet(f"s3a://bronze-data/raw_data")
+    df.head()
+
+    # Transform data
+    
+    
 
 if __name__ == "__main__":
     main()
