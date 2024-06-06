6b2b0a5ea9b48cc7a7125245732f47ede2e2137b
Drissdo185 <dothanhdat185@gmail.com>
2024-06-06 21:36:56 +0700

    update

--- batch_processing/script/bronze_to_silver.py
+++ batch_processing/script/bronze_to_silver.py
@@ -1,80 +1,81 @@
 from minio import Minio
-from pyspark.sql import SparkSession
-from pyspark.ml.feature import StringIndexer
 from minio.error import S3Error
+from pyspark.sql import SparkSession
 from pyspark.sql.functions import when
 import io
-from helpers import load_cfg 
+from helpers import load_cfg
 
 CFG_FILE = "config.yaml"
 
-def main():
-    cfg = load_cfg(CFG_FILE)
-    lakehouse_cfg = cfg["minio_lakehouse"]
-    datalake_cfg = cfg["minio_datalake"]
-
-    # Create a client with the MinIO server playground, its access key and secret key.
-    client = Minio(
-        endpoint=lakehouse_cfg["endpoint"],
-        access_key=lakehouse_cfg["access_key"],
-        secret_key=lakehouse_cfg["secret_key"],
-        secure=False,
+def create_minio_client(config):
+    """Create and return a MinIO client."""
+    return Minio(
+        endpoint=config["endpoint"],
+        access_key=config["access_key"],
+        secret_key=config["secret_key"],
+        secure=False
     )
 
-   # Create a bucket if it doesn't exist
-    found = client.bucket_exists(bucket_name=lakehouse_cfg["bucket_name"])
-    if not found:
-        client.make_bucket(bucket_name=lakehouse_cfg["bucket_name"])
+def ensure_bucket_exists(client, bucket_name):
+    """Ensure that the specified bucket exists."""
+    if not client.bucket_exists(bucket_name=bucket_name):
+        client.make_bucket(bucket_name=bucket_name)
     else:
-        print(f'Bucket {lakehouse_cfg["bucket_name"]} already exists, skip creating!')
+        print(f'Bucket {bucket_name} already exists, skip creating!')
 
-    # Create a folder in the bucket
-    folder_name = lakehouse_cfg["folder_name"] + "/"
+def create_folder_in_bucket(client, bucket_name, folder_name):
+    """Create a folder in the specified bucket."""
     try:
-        client.put_object(lakehouse_cfg["bucket_name"], folder_name, io.BytesIO(b''), 0)
-        print(f'Folder {folder_name} created in bucket {lakehouse_cfg["bucket_name"]}')
+        client.put_object(bucket_name, folder_name + "/", io.BytesIO(b''), 0)
+        print(f'Folder {folder_name} created in bucket {bucket_name}')
     except S3Error as err:
         print(f'Error: {err}')
 
-    # Spark session
-    spark = (SparkSession.builder.master("local")
-             .appName("Bronze to Silver")
-             .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.1,com.amazonaws:aws-java-sdk-bundle:1.11.375")
-             .config("spark.hadoop.fs.s3a.access.key", "minio_access_key")
-             .config("spark.hadoop.fs.s3a.secret.key", "minio_secret_key")
-             .config("spark.hadoop.fs.s3a.path.style.access", "true")
-             .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-             .config("spark.hadoop.fs.s3a.endpoint", "http://localhost:9000")
-             .getOrCreate())
+def create_spark_session(config):
+    """Create and return a Spark session with the given configuration."""
+    return ( SparkSession.builder.master("local[*]")
+    .config(
+        "spark.jars",
+        "../jars/aws-java-sdk-bundle-1.11.375.jar,../jars/hadoop-aws-3.2.0.jar",
+    )
+    .config("spark.hadoop.fs.s3a.endpoint", f'http://localhost:9000')
+    .config("spark.hadoop.fs.s3a.access.key", 'minio_access_key')
+    .config("spark.hadoop.fs.s3a.secret.key", 'minio_secret_key')
+    .config("spark.hadoop.fs.s3a.path.style.access", "true")
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    .config("spark.jars.packages", "io.delta:delta-core_2.12:1.2.1")
+    .config(
+        "spark.jars.repositories",
+        "https://maven-central.storage-download.googleapis.com/maven2/",
+    )
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+    .config(
+        "spark.sql.catalog.spark_catalog",
+        "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+    )
+    .config("spark.executor.memory", "18g")
+    .config("spark.driver.memory", "5g")
+    .appName("Python Spark transform Bronze to Silver")
+    .getOrCreate())
 
-    # Read data
-    df = spark.read.parquet(f"s3a://bronze-data/raw_data")
-    df.show()
 
-
-    # Transform data
-    # Label encoding with category columns
-    # With yes or no columns
+def transform_data(df):
+    """Perform data transformations on the DataFrame."""
     df = df.withColumn('default', when(df['default'] == 'yes', 1).otherwise(0))
     df = df.withColumn('housing', when(df['housing'] == 'yes', 1).otherwise(0))
     df = df.withColumn('loan', when(df['loan'] == 'yes', 1).otherwise(0))
-    # With other category columns
-    # Replace 'divorced' with '0', 'single' with '1', and 'married' with '2'
-    # in the 'marital' column
+    
     df = df.withColumn('marital', when(df['marital'] == 'divorced', 0)
                                     .when(df['marital'] == 'single', 1)
                                     .when(df['marital'] == 'married', 2)
                                     .otherwise(df['marital']))
 
-    # Replace 'unknown' with '0', 'primary' with '1', 'secondary' with '2', 
-    # and 'tertiary' with '3' in the 'education' column
-    df = df.withColumn('education', when(df['education'] == 'unknown', 0).
-                                        when(df['education'] == 'primary', 1).
-                                        when(df['education'] == 'secondary', 2).
-                                        when(df['education'] == 'tertiary', 3).
-                                        otherwise(df['education']))
+    df = df.withColumn('education', when(df['education'] == 'unknown', 0)
+                                        .when(df['education'] == 'primary', 1)
+                                        .when(df['education'] == 'secondary', 2)
+                                        .when(df['education'] == 'tertiary', 3)
+                                        .otherwise(df['education']))
 
-    # Replace job categories with numerical values in the 'job' column
     df = df.withColumn('job', when(df['job'] == 'admin.', 0)
                             .when(df['job'] == 'unknown', 1)
                             .when(df['job'] == 'unemployed', 2)
@@ -89,30 +90,45 @@ def main():
                             .when(df['job'] == 'services', 11)
                             .otherwise(df['job']))
 
-    # Replace 'unknown' with '0', 'telephone' with '1', and 'cellular' with '2'
-    # in the 'contact' column
     df = df.withColumn('contact', when(df['contact'] == 'unknown', 0)
                                 .when(df['contact'] == 'telephone', 1)
                                 .when(df['contact'] == 'cellular', 2)
                                 .otherwise(df['contact']))
 
-    # Replace 'unknown' with '0', 'other' with '1', 'failure' with '2', 
-    # and 'success' with '3' in the 'poutcome' column
     df = df.withColumn('poutcome', when(df['poutcome'] == 'unknown', 0)
                                     .when(df['poutcome'] == 'other', 1)
                                     .when(df['poutcome'] == 'failure', 2)
                                     .when(df['poutcome'] == 'success', 3)
                                     .otherwise(df['poutcome']))
 
-    # Set data type
-    
-    # Write delta table
-    df.write.format("delta").mode("overwrite").save(f"s3://{lakehouse_cfg['bucket_name']}/transformed_data")
+    return df
 
+def main():
+    # Load configuration
+    cfg = load_cfg(CFG_FILE)
+    lakehouse_cfg = cfg["minio_lakehouse"]
+    datalake_cfg = cfg["minio_datalake"]
 
-    
-    
-    
+    # Create MinIO client and ensure bucket and folder exist
+    client = create_minio_client(lakehouse_cfg)
+    ensure_bucket_exists(client, lakehouse_cfg["bucket_name"])
+    create_folder_in_bucket(client, lakehouse_cfg["bucket_name"], lakehouse_cfg["folder_name"])
+
+    # Create Spark session
+    spark = create_spark_session(datalake_cfg)
+
+    # Read data from the bronze bucket
+    bronze_file_path = f"s3a://bronze-data/raw_data"
+    df = spark.read.parquet(bronze_file_path)
+    df.show()
+
+    # Transform data
+    transformed_df = transform_data(df)
+    transformed_df.show()
+
+    # Write transformed data to the silver bucket in Delta Lake format
+    silver_file_path = f"s3a://silver-data/transformed_data"
+    transformed_df.write.format("delta").mode("overwrite").save(silver_file_path)
 
 if __name__ == "__main__":
     main()
